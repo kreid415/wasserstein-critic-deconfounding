e29f57fcc8687f08223c2c714832472354565450
kreid415 <kreid415@protonmail.com>
2025-11-08 16:17:48 +0000

    remove unneeded code

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,3 @@
-git clone https://github.com/kreid415/scvi-tools.git
-cd scvi-tools
-git checkout kr_dev
-pip install -e .
-cd ..
-
-pre-commit install
-
 pip install -e .
 
 pip install ipywidgets --upgrade
